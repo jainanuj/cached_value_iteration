1344e6b8a755bc407639ddff388e661589fa89de
Live session user <jainanuj99@gmail.com>
2019-11-11 07:14:57 +0000

    macpro-ubuntu-results and modified test script

--- run_tests.sh
+++ run_tests.sh
@@ -4,27 +4,27 @@ cp cache_aware_vi.c.Clust.Anneal cache_aware_vi.c
 cp solve.c.1MInitial solve.c
 make clean
 make
-./solve -p ../MDPs/evaluated/sap_900x900.mdp -method ftvi >op_sap_900x900_clust_anneal
-./solve -p ../MDPs/evaluated/mcar_1000x1000.mdp -method ftvi >op_mcar_1000x1000_clust_anneal
-./solve -p ../MDPs/evaluated/dap-100x100x10x10.mdp -method ftvi >op_dap-100x100x10x10_clust_anneal
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/sap_900x900.mdp -method ftvi >op_sap_900x900_clust_anneal 2>sap_900_clust_ann_perf
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/mcar_1000x1000.mdp -method ftvi >op_mcar_1000x1000_clust_anneal 2>mcar_1000_clust_ann_perf
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/dap-100x100x10x10.mdp -method ftvi >op_dap-100x100x10x10_clust_anneal 2>dap-100_clust_ann_perf
 #clust and 4M
 cp solve.c.4MInitial solve.c
 make clean
 make
-./solve -p ../MDPs/evaluated/mcar_2000x2000.mdp -method ftvi > op_mcar_2000x2000_clust_anneal
-./solve -p ../MDPs/evaluated/dap-200x100x10x10.mdp -method ftvi > op_dap-200x100x10x10_clust_anneal
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/mcar_2000x2000.mdp -method ftvi > op_mcar_2000x2000_clust_anneal 2>mcar2000_clust_ann_perf
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/dap-200x100x10x10.mdp -method ftvi > op_dap-200x100x10x10_clust_anneal 2>dap-200_clust_ann_perf
 
 #noclust and 1M
 cp cache_aware_vi.c.noClust.noAnneal cache_aware_vi.c
 cp solve.c.1MInitial solve.c
 make clean
 make
-./solve -p ../MDPs/evaluated/sap_900x900.mdp -method ftvi > op_sap_900x900_noclust
-./solve -p ../MDPs/evaluated/mcar_1000x1000.mdp -method ftvi > op_mcar_1000x1000_noclust
-./solve -p ../MDPs/evaluated/dap-100x100x10x10.mdp -method ftvi > op_dap-100x100x10x10_noclust
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/sap_900x900.mdp -method ftvi > op_sap_900x900_noclust 2>sap_900_noclust_perf
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/mcar_1000x1000.mdp -method ftvi > op_mcar_1000x1000_noclust 2>mcar_1000_noclust_perf
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/dap-100x100x10x10.mdp -method ftvi > op_dap-100x100x10x10_noclust 2>dap-100_noclust_perf
 #noclust and 4M
 cp solve.c.4MInitial solve.c
 make clean
 make
-./solve -p ../MDPs/evaluated/mcar_2000x2000.mdp -method ftvi >op_mcar_2000x2000_noclust
-./solve -p ../MDPs/evaluated/dap-200x100x10x10.mdp -method ftvi >op_dap-200x100x10x10_noclust
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/mcar_2000x2000.mdp -method ftvi >op_mcar_2000x2000_noclust 2>mcar_2000_noclust_perf
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/dap-200x100x10x10.mdp -method ftvi >op_dap-200x100x10x10_noclust 2>dap-200_noclust_perf
