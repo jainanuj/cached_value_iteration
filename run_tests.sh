b60b42c9acbe23150dea9baa92b07b5d2b49817c
Anuj Jain <ajain@dwarf.grandenetworks.net>
2019-11-12 00:52:33 -0600

    Adding clust noAnn runs

--- run_tests.sh
+++ run_tests.sh
@@ -14,6 +14,21 @@ make
 perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/mcar_2000x2000.mdp -method ftvi > op_mcar_2000x2000_clust_anneal 2>mcar2000_clust_ann_perf
 perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/dap-200x100x10x10.mdp -method ftvi > op_dap-200x100x10x10_clust_anneal 2>dap-200_clust_ann_perf
 
+#clustnoann and 1M
+cp cache_aware_vi.c.clust.noAnneal cache_aware_vi.c
+cp solve.c.1MInitial solve.c
+make clean
+make
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/sap_900x900.mdp -method ftvi >op_sap_900x900_clust_noanneal 2>sap_900_clust_noann_perf
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/mcar_1000x1000.mdp -method ftvi >op_mcar_1000x1000_clust_noanneal 2>mcar_1000_clust_noann_perf
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/dap-100x100x10x10.mdp -method ftvi >op_dap-100x100x10x10_clust_noanneal 2>dap-100_clust_noann_perf
+#clust and 4M
+cp solve.c.4MInitial solve.c
+make clean
+make
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/mcar_2000x2000.mdp -method ftvi > op_mcar_2000x2000_clust_noanneal 2>mcar2000_clust_noann_perf
+perf stat -e task-clock,cycles,instructions,cache-references,cache-misses ./solve -p ../MDPs/evaluated/dap-200x100x10x10.mdp -method ftvi > op_dap-200x100x10x10_clust_noanneal 2>dap-200_clust_noann_perf
+
 #noclust and 1M
 cp cache_aware_vi.c.noClust.noAnneal cache_aware_vi.c
 cp solve.c.1MInitial solve.c
