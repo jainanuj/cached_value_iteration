9e9c18474a7fc8825bacaf3325a28466b6b6c9d6
Anuj Jain <ajain@dwarf.grandenetworks.net>
2020-03-21 00:01:24 -0500

    Changing the partition size to 1300

--- types.h
+++ types.h
@@ -12,9 +12,9 @@
 #include "intqueue.h"
 #include "intheap.h"
 
-#define PART_SIZE  5000//6100       //3
+#define PART_SIZE  1300//6100       //3
 #define ARR_SIZE  1000000
-#define NUM_PARTS_IN_LEVEL1 1000
+#define NUM_PARTS_IN_LEVEL1 10000
 
 
 typedef struct vec_t {
